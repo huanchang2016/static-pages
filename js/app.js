970e19d05fc8403d08e8bb3df5b82662fd8060b7
huanchang2016 <380019340@qq.com>
2017-11-03 00:51:35 +0800

    content-right  and  link footer and so on...

--- js/app.js
+++ js/app.js
@@ -12,11 +12,11 @@ $(function(){
 
 	// banner 轮播动画效果
 	var $lis = $(".banner-list li"),
-		len = $lis.length,
-		currentIndex = 0,
-		nextIndex = 1,
-		_html = '',
-		_time = null;
+	len = $lis.length,
+	currentIndex = 0,
+	nextIndex = 1,
+	_html = '',
+	_time = null;
 
 	// 创建小圆点span
 	for(var i=0;i<len;i++){
@@ -26,12 +26,12 @@ $(function(){
 	$(".circle").eq(0).html(_html);
 
 	$(".circle span").eq(0).addClass("b-active").end()
-				   .on("click",function(){
-				   		if($(this).index() === currentIndex)
-				   			return;
-				   		nextIndex = $(this).index();
-				   		move();
-				   });
+	.on("click",function(){
+		if($(this).index() === currentIndex)
+			return;
+		nextIndex = $(this).index();
+		move();
+	});
 	
 	// 设置自动的淡入淡出轮播效果
 	$(".b-box").hover(function(){
@@ -48,7 +48,7 @@ $(function(){
 
 		// 改变小圆点样式
 		$(".circle span").eq(nextIndex).addClass("b-active")
-					   .siblings().removeClass("b-active");
+		.siblings().removeClass("b-active");
 		currentIndex = nextIndex;
 		nextIndex++;
 		if(nextIndex === len){
@@ -63,4 +63,78 @@ $(function(){
 		$(_tabs).hide();
 		$(_tabs).eq(_i).show();
 	});
+
+// 互动 轮播
+
+	var lis = $(".c-slider li"),
+	liWidth = $(lis[0]).width(),
+	len = lis.length,
+	_index = 1,
+	firstNode = lis[0].cloneNode(true); // 克隆第一个节点
+
+	// 将克隆的第一个节点添加到最后
+	$(".c-box")[0].appendChild(firstNode);
+	len += 1;
+	// 设置 ul.c-box 的宽度
+	$(".c-box")[0].style.width = (liWidth * len) + "px";
+
+	// 自动轮播
+	setInterval(function(){
+		var _left = -1 * _index * liWidth;
+		_index++;				
+		animate($(".c-box")[0], {left:_left}, function(){
+			// 运动结束，执行函数
+			// 判断是否显示到最后一张图片
+			if (_index === len){
+				$(".c-box")[0].style.left = 0;
+				_index = 1;
+			}
+		});	
+	}, 2000);
+
+
+	// 运动函数
+// element : 待实现运动动画的元素
+// options : 运动属性的配置对象
+// speed : 运动时间
+// fn : 函数，运动动画执行结束后，还需要再执行的函数
+function animate(element, options, speed, fn) {
+	if (typeof speed === "function"  || typeof speed === "undefined"){
+		fn = speed;
+		speed = 400;
+	}
+
+	// 先取消在 element 元素上的运动计时器
+	clearInterval(element.timer);
+
+	// 存放各属性起始值的对象
+	var startPosition = {};
+	for (var attr in options) {
+		startPosition[attr] = parseFloat(css(element, attr));
+	}
+	var startTime = +new Date(); // 记录开始运动的时间
+	// 启动计时器，实现运动效果
+	element.timer = setInterval(function(){
+		var elapse = Math.min(+new Date() - startTime, speed); // 运动时间差
+		// 循环为每个属性值赋新计算值
+		// 线性运动公式：(终值-初值) / 总时间 * 运动耗时 + 初值
+		for (var attr in options) {
+			var val = (options[attr] - startPosition[attr]) / speed * elapse + startPosition[attr];
+			element.style[attr] = val + (attr === "opacity" ? "" : "px");
+			if (attr === "opacity")
+				element.style.filter = "alpha(opacity="+ (val * 100) +")";
+		}
+		if (elapse === speed){ // 到达运动总时间，则停止计时器
+			clearInterval(element.timer);
+			// 运动执行结束后需要继续执行的函数
+			fn && fn();
+		}
+	}, 30);
+}
+
+// 处理获取节点元素的额属性值的兼容
+	function css(obj, attr){
+		return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj, null)[attr];
+	}
+
 })
